1f930fcd06b00934562931d8e33586c01fa4d8c3
yosefabasheli <147565066+yosefabasheli@users.noreply.github.com>
2023-12-12 22:04:14 -0600

    Alter design

---
+++ index.js
@@ -0,0 +1,29 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let password1Disp = document.querySelector("#password1");
+let password2Disp = document.querySelector("#password2");
+
+let passlength = 15;
+
+ 
+ function randomCh() {
+     let randomNo = Math.floor(Math.random() * characters.length);
+     return characters[randomNo];
+ }
+ 
+ function genrateCh() {
+      let answer= " " 
+     for (let i = 0; i < passlength; i++){
+       answer += randomCh();
+       
+     }
+    return answer;
+ }
+ const password1 = genrateCh();
+ password1Disp.textContent = password1;
+ 
+ const password2 = genrateCh();
+ password2Disp.textContent = password2;
+ 
+ 
